3270bda50b2f9895a2834b4eabe00bcd6d346898
Systemallica <andressser@gmail.com>
2018-01-23 19:13:03 +0100

    Initial interface

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'katas#index'
   
-  get '/kata/:id' => 'katas#show', as: :kata
+  get '/katas/:id' => 'katas#show', as: :kata
 end
--- spec/features/katas_spec.rb
+++ spec/features/katas_spec.rb
@@ -2,19 +2,24 @@ require 'rails_helper'
 require 'capybara'
 
 describe 'Kata' do
-	it 'can be read' do
-		kata_title = 'Kata title'
-		kata_description = 'Kata description'
-
-		kata = Kata.new(
-			title: kata_title,
-			description: kata_description
-			)
-		kata.save
+	
+	it 'can be read jeje' do
+		kata_title = 'Second title'
+		kata_description = 'Second description'
+		create_kata
+		create_kata(title: kata_title, description: kata_description)
 
 		visit root_path
 		click_on(kata_title)
+
 		expect(page).to have_content(kata_title)
 		expect(page).to have_content(kata_description)
+	end
+
+	def create_kata(title: 'Kata title', description: 'May the force be with you')
+		kata = Kata.new(title: title, description: description)
+		kata.save
+
+		kata
 	end
 end
