9e9d2d9c39285a37b632c58e38b51bdd467aa5ea
Systemallica <andressser@gmail.com>
2018-01-23 20:25:58 +0100

    Added option to create Katas

--- spec/features/katas_spec.rb
+++ spec/features/katas_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 require 'capybara'
 
-describe 'Kata' do
-	
+describe 'Kata' do	
 	it 'can be read jeje' do
 		kata_title = 'Second title'
 		kata_description = 'Second description'
@@ -11,6 +10,20 @@ describe 'Kata' do
 
 		visit root_path
 		click_on(kata_title)
+
+		expect(page).to have_content(kata_title)
+		expect(page).to have_content(kata_description)
+	end
+
+	it 'can be created' do
+		kata_title = 'Kata Sith'
+		kata_description = 'Kata kill'
+
+		visit root_path
+		click_on('New Kata')
+		fill_in(:title, with: kata_title)
+		fill_in(:description, with: kata_description)
+		click_on('Save')
 
 		expect(page).to have_content(kata_title)
 		expect(page).to have_content(kata_description)
---
+++ spec/models/kata_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'Kata' do 
+	it 'needs a title' do
+		kata = Kata.new(
+			title: nil,
+			description: 'any'
+			)
+		kata.save
+
+		expect(kata.errors.size).to eq(1)
+	end
+
+	it 'needs a description' do
+		kata = Kata.new(
+			title: 'any',
+			description: nil
+			)
+		kata.save
+
+		expect(kata.errors.size).to eq(1)
+	end 
+end
