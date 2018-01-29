a263e6831504b41dbfaef614c22b99a66f5ce91b
Systemallica <andressser@gmail.com>
2018-01-29 19:41:59 +0100

    Controller the rails way

--- app/controllers/katas_controller.rb
+++ app/controllers/katas_controller.rb
@@ -1,46 +1,52 @@
 class KatasController < ApplicationController
+	before_action :set_kata, only:[:show, :edit, :update, :destroy]
+
 	def index
 		@katas = Kata.all
 	end
 
 	def show
-		@kata = Kata.find(params[:id])
 	end
 
 	def new
+		@kata = Kata.new
 	end
 
 	def create
-		kata = Kata.new(
-			title: params[:title], 
-			description: params[:description]
-			)
-		if kata.save
-			redirect_to kata_path(kata.id)
+		@kata = Kata.new(kata_params)
+		if @kata.save
+			redirect_to kata_path(@kata.id)
 		else
 			render :new
 		end
 	end
 
 	def edit
-		@kata = Kata.find(params[:id])
 	end
 
 	def update
-		kata = Kata.find(params[:id])
-		kata.title = params[:kata][:title] 
-		kata.description = params[:kata][:description]
-		if kata.save
-			redirect_to kata_path(kata.id)
-		else
-			render :new
-		end
+		@kata.update(
+			title: params[:kata][:title], 
+			description: params[:kata][:description]
+			)
+
+		redirect_to kata_path(@kata.id)
 	end
 
 	def destroy
-		kata = Kata.find(params[:id])
-		kata.destroy
+		@kata.destroy
 
 		redirect_to root_path
 	end
+
+	private
+
+	def set_kata
+		@kata = Kata.find(params[:id])
+	end
+
+	def kata_params
+		params.require(:kata).permit(:title, :description)
+	end
+
 end
